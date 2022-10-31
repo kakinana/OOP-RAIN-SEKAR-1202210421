cbe9d362091c6d6bb5ff264508f11c699016f4e2
kakinana <kakinana@student.telkomuniversity.ac.id>
2022-10-31 08:31:32 +0700

    first commit

---
+++ src/Menu.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        database db = new database ();
+        Menu menu1 = new Menu ();
+        menu1.setname ("Bakso");
+        menu1.setcategory ("Makanan");
+        Menu menu2 = new Menu ();
+        menu2.setname ("Es Teh");
+        menu2.setcategory ("Minuman");
+        Menu menu3 = new Menu;
+        menu3.setname ("Brownies");
+        menu3.setcategory ("Dessert");
+        
+        System.out.println("Selamat Datang di Restoran EAD\nSilakan Register Terlebih Dahulu\n==============================");
+    
+    public string nama = "Sekar_SI4505";
+        return nama;
+    public int nom = "1202210421";
+        return nom;
+    
+    System.out.println("Register Success\nName: \nPhone Number: \n==============================".nama.nom);
+        
+        while InsertMenu ();
+        Menu menu11.setname ("1. Menu");
+        Menu menu12.setname ("2. Pilih Menu");
+        Menu menu13.setname ("3. Keluar");
+
+
+
+   
+
+
+    
+
+        
+
+    }
+}
